42e769cf51bdc2e908c7901d5ad09d856cbfab6c
marijas <marijas@plusplus.rs>
2019-01-14 11:22:29 +0100

    Config service authorization

---
+++ course-service/src/main/resources/database/data/scripts/department_data.sql
@@ -0,0 +1,8 @@
+insert into department (name) values ('Katedra za informacione sisteme');
+insert into department (name) values ('Katedra za softversko inzenjerstvo');
+insert into department (name) values ('Katedra za ekonomiju, poslovno planiranje i medjunarodni menadzment');
+insert into department (name) values ('Katedra za elektronsko poslovanje');
+insert into department (name) values ('Katedra za industrisko i menadzment inzenjerstvo');
+insert into department (name) values ('Katedra za informacione tehnologije');
+insert into department (name) values ('Katedra za matematiku');
+insert into department (name) values ('Katedra za marketing menadzment i odnose sa javnoscu');
